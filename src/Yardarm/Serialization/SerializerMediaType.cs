002cf3762adf7b7070d46f076e8993c9cf5f2d8f
Brant Burnett <bburnett@centeredgesoftware.com>
2020-09-05 18:55:40 -0400

    Add equality handling to SerializerMediaType

--- src/Yardarm/Serialization/SerializerMediaType.cs
+++ src/Yardarm/Serialization/SerializerMediaType.cs
@@ -2,7 +2,7 @@
 
 namespace Yardarm.Serialization
 {
-    public class SerializerMediaType
+    public class SerializerMediaType : IEquatable<SerializerMediaType>
     {
         public string MediaType { get; }
 
@@ -18,5 +18,42 @@ namespace Yardarm.Serialization
             MediaType = mediaType ?? throw new ArgumentNullException(nameof(mediaType));
             Quality = quality;
         }
+
+        public bool Equals(SerializerMediaType? other)
+        {
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return MediaType == other.MediaType && Quality.Equals(other.Quality);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return Equals((SerializerMediaType) obj);
+        }
+
+        public override int GetHashCode() => HashCode.Combine(MediaType, Quality);
     }
 }

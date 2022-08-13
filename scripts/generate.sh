b06f702da5dce0d2e1b1cf9fc51dbeddfb7d8ab9
Brant Burnett <bburnett@centeredgesoftware.com>
2022-08-13 19:10:15 -0400

    Switch to MashTub, GitHub API has other issues

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-mkdir -p bin
-curl -sSL https://github.com/github/rest-api-description/releases/download/v1.1.4/descriptions.zip -o ./bin/descriptions.zip
-unzip -j bin/descriptions.zip descriptions/api.github.com/api.github.com.json -d ./bin/
+mkdir -p ./bin
+curl -sSL https://api.adv.centeredge.io/v1/swagger/api/swagger.json -o ./bin/mashtub.json
 
-dotnet run --no-launch-profile --project src/Yardarm.CommandLine -- generate -n TestSTJ -x src/Yardarm.SystemTextJson/bin/Release/net6.0/Yardarm.SystemTextJson.dll -f netstandard2.0 net6.0 --embed -o ./bin -v 1.0.0 -i ./bin/api.github.com.json
+dotnet run --no-build --no-launch-profile -c Release --project src/Yardarm.CommandLine -- \
+    generate -n TestSTJ -x src/Yardarm.SystemTextJson/bin/Release/net6.0/Yardarm.SystemTextJson.dll -f netstandard2.0 net6.0 --embed --nupkg ./bin/ -v 1.0.0 -i ./bin/mashtub.json
 
-dotnet run --no-launch-profile --project src/Yardarm.CommandLine -- generate -n TestNJ -x src/Yardarm.NewtonsoftJson/bin/Release/net6.0/Yardarm.NewtonsoftJson.dll -f netstandard2.0 net6.0 --embed -o ./bin -v 1.0.0 -i ./bin/api.github.com.json
+dotnet run --no-build --no-launch-profile -c Release --project src/Yardarm.CommandLine -- \
+    generate -n TestNJ -x src/Yardarm.NewtonsoftJson/bin/Release/net6.0/Yardarm.NewtonsoftJson.dll -f netstandard2.0 net6.0 --embed --nupkg ./bin/ -v 1.0.0 -i ./bin/mashtub.json

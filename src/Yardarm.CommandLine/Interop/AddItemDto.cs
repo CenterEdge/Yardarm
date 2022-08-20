caa3c22f31d4481704ce1aa969376f27bb9c696d
Brant Burnett <bburnett@centeredgesoftware.com>
2022-08-20 18:52:40 -0400

    Add collect-package-references command
    Motivation
    ----------
    Provide an extension point that outputs package references in a format
    which can be read by an MSBuild task. This will allow NuGet restore
    from an MSBuild project to know the correct packages to restore.
    
    Modifications
    -------------
    Add the command and DTOs for serializing JSON information on the console
    to be received by MSBuild.

---
+++ src/Yardarm.CommandLine/Interop/AddItemDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+#nullable enable
+
+namespace Yardarm.CommandLine.Interop
+{
+    // Note: DataContract attributes are used for deserialization within legacy framework MSBuild
+
+    [DataContract]
+    internal class AddItemDto
+    {
+        [DataMember(Name = "itemType")]
+        public string? ItemType { get; set; }
+
+        [DataMember(Name = "targetFramework")]
+        public string? TargetFramework { get; set; }
+
+        [DataMember(Name = "identity")]
+        public string? Identity { get; set; }
+
+        [DataMember(Name = "metadata")]
+        public Dictionary<string, string>? Metadata { get; set; }
+    }
+}
---
+++ src/Yardarm.CommandLine/Interop/DtoSerializerContext.cs
@@ -0,0 +1,17 @@
+﻿#if NET6_0_OR_GREATER
+
+using System;
+using System.Text.Json.Serialization;
+
+#nullable enable
+
+namespace Yardarm.CommandLine.Interop
+{
+    [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+    [JsonSerializable(typeof(AddItemDto))]
+    internal partial class DtoSerializerContext : JsonSerializerContext
+    {
+    }
+}
+
+#endif

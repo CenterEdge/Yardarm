3321aad4d9edb99df06e562333cea0c00ed391a7
Brant Burnett <bburnett@centeredgesoftware.com>
2022-08-21 12:05:27 -0400

    Create MSBuild tasks for Yardarm
    Motivation
    ----------
    Provide MSBuild tasks which can execute collect-dependencies and
    generate commands.
    
    Modifications
    -------------
    - Move existing solution to the "main" subdirectory.
    - Add Yardarm.Build.Tasks to a new "sdk" subdirectory.

---
+++ src/sdk/Yardarm.Build.Tasks/YardarmCollectDependencies.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Yardarm.CommandLine.Interop;
+
+#if NETCOREAPP
+using System.Text.Json;
+#else
+using System.IO;
+using System.Runtime.Serialization.Json;
+#endif
+
+namespace Yardarm.Build.Tasks;
+
+public class YardarmCollectDependencies : YardarmCommonTask
+{
+    private const string AddItemPrefix = "AddItem: ";
+
+#if NETCOREAPP
+    private static readonly JsonSerializerOptions s_serializerOptions = new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    };
+#else
+    private static readonly DataContractJsonSerializer s_serializer = new(typeof(AddItemDto), new DataContractJsonSerializerSettings
+    {
+        UseSimpleDictionaryFormat = true
+    });
+#endif
+
+    protected override string Verb => "collect-dependencies";
+
+    private readonly List<ITaskItem> _packageReference = new();
+    private readonly List<ITaskItem> _packageDownload = new();
+    private readonly List<ITaskItem> _frameworkReference = new();
+
+    [Output]
+    public ITaskItem[] PackageReference => _packageReference.ToArray();
+
+    [Output]
+    public ITaskItem[] PackageDownload => _packageDownload.ToArray();
+
+    [Output]
+    public ITaskItem[] FrameworkReference => _frameworkReference.ToArray();
+
+    protected override void LogEventsFromTextOutput(string singleLine, MessageImportance messageImportance)
+    {
+        if (!singleLine.StartsWith(AddItemPrefix))
+        {
+            base.LogEventsFromTextOutput(singleLine, messageImportance);
+            return;
+        }
+
+        try
+        {
+            singleLine = singleLine.Substring(AddItemPrefix.Length);
+
+#if NETCOREAPP
+            var item = JsonSerializer.Deserialize<AddItemDto>(singleLine, s_serializerOptions)!;
+#else
+            // Since we need to be compatible with net472 and don't want to take dependencies, we must engage in this ugliness
+            using var stream = new MemoryStream(Encoding.UTF8.GetBytes(singleLine.Substring(AddItemPrefix.Length)));
+            var item = (AddItemDto) s_serializer.ReadObject(stream);
+#endif
+
+            if (!string.IsNullOrWhiteSpace(item.Identity))
+            {
+                var taskItem = new TaskItem(item.Identity);
+
+                if (item.Metadata is not null)
+                {
+                    foreach (var metadata in item.Metadata)
+                    {
+                        taskItem.SetMetadata(metadata.Key, metadata.Value);
+                    }
+                }
+
+                switch (item.ItemType)
+                {
+                    case "PackageReference":
+                        _packageReference.Add(taskItem);
+                        break;
+
+                    case "PackageDownload":
+                        _packageDownload.Add(taskItem);
+                        break;
+
+                    case "FrameworkReference":
+                        _frameworkReference.Add(taskItem);
+                        break;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Log.LogErrorFromException(ex, false);
+        }
+    }
+}
---
+++ src/sdk/Yardarm.Build.Tasks/YardarmTask.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Yardarm.Build.Tasks
+{
+    public abstract class YardarmTask : ToolTask
+    {
+        private static readonly Regex s_errorRegex = new Regex(@"^\[(\w+)\] (.+)");
+
+        protected override string GenerateFullPathToTool() => ToolName;
+
+        protected override string ToolName => "Yardarm.CommandLine.exe";
+
+        protected override void LogEventsFromTextOutput(string singleLine, MessageImportance messageImportance)
+        {
+            var match = s_errorRegex.Match(singleLine);
+
+            if (match.Success)
+            {
+                var type = match.Groups[1].Value;
+                var message = match.Groups[2].Value;
+
+                switch (type)
+                {
+                    case "ERR":
+                        Log.LogError(message);
+                        break;
+
+                    case "WRN":
+                        Log.LogWarning(message);
+                        break;
+
+                    default:
+                        Log.LogMessage(messageImportance, message);
+                        break;
+                }
+            }
+            else
+            {
+                Log.LogMessage(messageImportance, singleLine);
+            }
+        }
+    }
+}

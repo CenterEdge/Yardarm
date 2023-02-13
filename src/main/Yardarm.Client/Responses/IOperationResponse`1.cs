26b8a32b04f40f89534d5928bd056ed2fca1cf4c
Brant Burnett <bburnett@centeredgesoftware.com>
2023-02-13 10:53:36 -0500

    Add `IOperationResponse<TBody>` to responses with a body (#164)
    Motivation
    ----------
    When dealing with abstraction layers wrapping multiple different API calls it is very difficult to gain access to the body as each different response object as a different `GetBodyAsync` method returning a different type.
    
    Modifications
    -------------
    Add a common `IOperationResponse<TBody>` interface, inherited from `IOperationResponse`, which includes the `GetBodyAsync` method. Include this interface on all response classes which include a body.
    
    Results
    -------
    Abstraction layers in consumers can use type checking to find the interface they want. This is particularly useful for error handling if the API in question returns all errors with an `Error` schema or similar. You can simply `response as IOperationResponse<Error>` to test and then get the `Error` object from the `GetBodyAsync` method.
    
    (cherry picked from commit 2ce392cde21291c9c0843deac151b0b99fd17500)

---
+++ src/main/Yardarm.Client/Responses/IOperationResponse`1.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+// ReSharper disable once CheckNamespace
+namespace RootNamespace.Responses
+{
+    /// <summary>
+    /// An operation response with a specific body schema.
+    /// </summary>
+    /// <typeparam name="TBody">The type of the body schema.</typeparam>
+    public interface IOperationResponse<TBody> : IOperationResponse
+    {
+        /// <summary>
+        /// Get the body of the response.
+        /// </summary>
+        /// <returns>The deserialized body.</returns>
+        ValueTask<TBody> GetBodyAsync();
+    }
+}

839fe3fb67fc9a6b4b482e3c337a1ad6ca872ab6
Brant Burnett <bburnett@centeredgesoftware.com>
2023-05-07 12:28:32 -0400

    Apply correct TargetRuntime attribute to assemblies (#188)

--- src/main/Yardarm/Enrichment/Compilation/TargetRuntimeAssemblyInfoEnricher.cs
+++ src/main/Yardarm/Enrichment/Compilation/TargetRuntimeAssemblyInfoEnricher.cs
@@ -1,21 +1,48 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using NuGet.Frameworks;
 using Yardarm.Helpers;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace Yardarm.Enrichment.Compilation
 {
     public class TargetRuntimeAssemblyInfoEnricher : IAssemblyInfoEnricher
     {
+        private readonly GenerationContext _generationContext;
+
+        public TargetRuntimeAssemblyInfoEnricher(GenerationContext generationContext)
+        {
+            ArgumentNullException.ThrowIfNull(generationContext);
+            _generationContext = generationContext;
+        }
+
         public CompilationUnitSyntax Enrich(CompilationUnitSyntax syntax) => syntax
-            .AddAttributeLists(
-                SyntaxFactory.AttributeList().AddAttributes(
-                    SyntaxFactory.Attribute(SyntaxFactory.ParseName("System.Runtime.Versioning.TargetFramework"))
-                        .AddArgumentListArguments(
-                            SyntaxFactory.AttributeArgument(SyntaxHelpers.StringLiteral(".NETStandard,Version=v2.0")),
-                            SyntaxFactory.AttributeArgument(SyntaxHelpers.StringLiteral(""))
-                                .WithNameEquals(SyntaxFactory.NameEquals("FrameworkDisplayName"))))
-                    .WithTarget(SyntaxFactory.AttributeTargetSpecifier(SyntaxFactory.Token(SyntaxKind.AssemblyKeyword)))
-                    .WithTrailingTrivia(SyntaxFactory.ElasticCarriageReturnLineFeed));
+            .AddAttributeLists(AttributeList(
+                    AttributeTargetSpecifier(Token(SyntaxKind.AssemblyKeyword)),
+                    SingletonSeparatedList(Attribute(
+                        ParseName("System.Runtime.Versioning.TargetFramework"),
+                        AttributeArgumentList(SeparatedList(new[]
+                        {
+                            AttributeArgument(SyntaxHelpers.StringLiteral(_generationContext.CurrentTargetFramework.DotNetFrameworkName)),
+                            AttributeArgument(
+                                    NameEquals("FrameworkDisplayName"),
+                                    null,
+                                    SyntaxHelpers.StringLiteral(GetDisplayName(_generationContext.CurrentTargetFramework)))
+                        })))))
+                .WithTrailingTrivia(ElasticCarriageReturnLineFeed));
+
+        // This appears to align with example builds which don't include a display name for .NET Standard or .NET Core before .NET 5
+        private static string GetDisplayName(NuGetFramework framework) => framework switch
+        {
+            {Framework: ".NETCoreApp", Version.Major: >= 5} =>
+                $".NET {framework.Version.Major}.{framework.Version.Minor}",
+            {Framework: ".NETFramework", Version.Revision: > 0} =>
+                $".NET Framework {framework.Version.Major}.{framework.Version.Minor}.{framework.Version.Revision}",
+            {Framework: ".NETFramework"} =>
+                $".NET Framework {framework.Version.Major}.{framework.Version.Minor}",
+            _ => ""
+        };
     }
 }

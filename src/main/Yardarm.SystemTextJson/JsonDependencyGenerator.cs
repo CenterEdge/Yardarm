965bc69558af4e0aa815be8332a4a9131c894a18
Brant Burnett <bburnett@centeredgesoftware.com>
2023-11-18 08:52:27 -0500

    Upgrade to release bits of System.Text.Json 8.0

--- src/main/Yardarm.SystemTextJson/JsonDependencyGenerator.cs
+++ src/main/Yardarm.SystemTextJson/JsonDependencyGenerator.cs
@@ -21,7 +21,7 @@ namespace Yardarm.SystemTextJson
                     {
                         Name = "System.Text.Json",
                         TypeConstraint = LibraryDependencyTarget.Package,
-                        VersionRange = VersionRange.Parse("8.0.0-rc.2.23479.6")
+                        VersionRange = VersionRange.Parse("8.0.0")
                     }
                 };
 
@@ -31,7 +31,7 @@ namespace Yardarm.SystemTextJson
                     {
                         Name = "System.Net.Http.Json",
                         TypeConstraint = LibraryDependencyTarget.Package,
-                        VersionRange = VersionRange.Parse("8.0.0-rc.2.23479.6")
+                        VersionRange = VersionRange.Parse("8.0.0")
                     }
                 };
             }

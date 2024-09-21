2309bd33da6da5dc5bf896732118ef2a92f5fb0b
Brant Burnett <bburnett@centeredgesoftware.com>
2024-09-21 18:10:57 -0400

    Load and parse embedded resources more efficiently (#254)
    Motivation
    ----------
    The current approach of a string replace incurs some additional allocation overhead.
    
    Modifications
    -------------
    Process the string replacement as UTF8 against the raw bytes of the manifest resource stream rather than deserializing to a string and then generating another string with the namespace replaced.

--- src/main/Yardarm/Generation/ResourceSyntaxTreeGenerator.cs
+++ src/main/Yardarm/Generation/ResourceSyntaxTreeGenerator.cs
@@ -1,14 +1,12 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Text;
-using Yardarm.Generation.Internal;
 using Yardarm.Helpers;
+using Yardarm.Internal;
 using Yardarm.Names;
 using Yardarm.Packaging;
 
@@ -16,6 +14,9 @@ namespace Yardarm.Generation
 {
     public abstract class ResourceSyntaxTreeGenerator : ISyntaxTreeGenerator
     {
+        private static readonly UTF8Encoding s_utf8NoBom = new(encoderShouldEmitUTF8Identifier: false);
+        private static ReadOnlySpan<byte> RootNamespaceBytes => "RootNamespace"u8;
+
         public GenerationContext GenerationContext { get; }
         public IRootNamespace RootNamespace { get; }
 
@@ -37,21 +38,40 @@ namespace Yardarm.Generation
                 ? ".netcoreapp.cs"
                 : ".netstandard.cs";
 
-            return GetType().Assembly.GetManifestResourceNames()
-                .Where(p => p.StartsWith(ResourcePrefix) && p.EndsWith(".cs") && !p.EndsWith(excludeSuffix))
-                .Select(ParseResource);
+            byte[] namespaceName = s_utf8NoBom.GetBytes(RootNamespace.Name.ToString());
+
+            var result = new List<SyntaxTree>();
+            foreach (string resourceName in GetType().Assembly.GetManifestResourceNames())
+            {
+                if (resourceName.StartsWith(ResourcePrefix) && resourceName.EndsWith(".cs") && !resourceName.EndsWith(excludeSuffix))
+                {
+                    result.Add(ParseResource(resourceName, namespaceName));
+                }
+            };
+
+            return result;
         }
 
-        private SyntaxTree ParseResource(string resourceName)
+        private unsafe SyntaxTree ParseResource(string resourceName, ReadOnlySpan<byte> namespaceName)
         {
-            using var stream = GetType().Assembly
-                .GetManifestResourceStream(resourceName);
-            using var reader = new StreamReader(stream!, Encoding.UTF8);
+            using var stream = (UnmanagedMemoryStream)GetType().Assembly
+                .GetManifestResourceStream(resourceName)!;
+            if (stream.Length > int.MaxValue)
+            {
+                throw new InvalidOperationException("Resource is too large to parse");
+            }
 
-            string rawText = reader.ReadToEnd();
-            rawText = rawText.Replace("RootNamespace", RootNamespace.Name.ToString());
+            int length = (int)stream.Length;
+            ReadOnlySpan<byte> resourceContents = new(stream.PositionPointer, length);
 
-            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(SourceText.From(rawText, Encoding.UTF8),
+            SourceText sourceText = resourceContents.TryCopyWithReplace(RootNamespaceBytes, namespaceName, out ArrayPoolBuffer<byte> buffer)
+                ? SourceText.From(buffer.Buffer!, buffer.Length, s_utf8NoBom, canBeEmbedded: true)
+                : SourceText.From(stream, s_utf8NoBom, canBeEmbedded: true);
+
+            // Return the buffer, if any, to the array pool
+            buffer.Dispose();
+
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(sourceText,
                 CSharpParseOptions.Default
                     .WithLanguageVersion(LanguageVersion.CSharp12)
                     .WithPreprocessorSymbols(GenerationContext.PreprocessorSymbols),
---
+++ src/main/Yardarm/Internal/YardarmMemoryExtensions.cs
@@ -0,0 +1,85 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// TryCopyAndReplace is a variant based on String.Replace in .NET.
+
+using System;
+using System.Buffers;
+using System.Diagnostics;
+
+namespace Yardarm.Internal
+{
+    internal static class YardarmMemoryExtensions
+    {
+        /// <summary>
+        /// Copies the contents of the source to a new buffer, replacing all occurrences of oldValue with newValue,
+        /// if there is at least one instance of oldValue found.
+        /// </summary>
+        /// <typeparam name="T">Type of element.</typeparam>
+        /// <param name="source">Source buffer to search and copy.</param>
+        /// <param name="oldValue">Value to replace in the source buffer.</param>
+        /// <param name="newValue">Value to place in locations where <paramref name="oldValue"/> is found.</param>
+        /// <param name="result">Buffer backed by the <see cref="ArrayPool{T}"/> if replacements were made.</param>
+        /// <returns>True if a copy with replacements was placed in <paramref name="result"/>.</returns>
+        public static bool TryCopyWithReplace<T>(this ReadOnlySpan<T> source, ReadOnlySpan<T> oldValue,
+            ReadOnlySpan<T> newValue, out ArrayPoolBuffer<T> result)
+            where T : IEquatable<T>?
+        {
+            var replacementIndices = new ValueListBuilder<int>(stackalloc int[32]);
+
+            // Find all occurrences of the oldValue
+            int i = 0;
+            while (true)
+            {
+                int pos = source[i..].IndexOf(oldValue);
+                if (pos < 0)
+                {
+                    break;
+                }
+
+                replacementIndices.Append(i + pos);
+                i += pos + oldValue.Length;
+            }
+
+            if (replacementIndices.Length == 0)
+            {
+                result = default;
+                return false;
+            }
+
+            int length = source.Length + (newValue.Length - oldValue.Length) * replacementIndices.Length;
+            var dest = ArrayPool<T>.Shared.Rent(length);
+
+            Span<T> dstSpan = dest.AsSpan(0, length);
+
+            int thisIdx = 0;
+            int dstIdx = 0;
+
+            for (int r = 0; r < replacementIndices.Length; r++)
+            {
+                int replacementIdx = replacementIndices[r];
+
+                // Copy over the non-matching portion of the original that precedes this occurrence of oldValue.
+                int count = replacementIdx - thisIdx;
+                if (count != 0)
+                {
+                    source.Slice(thisIdx, count).CopyTo(dstSpan[dstIdx..]);
+                    dstIdx += count;
+                }
+                thisIdx = replacementIdx + oldValue.Length;
+
+                // Copy over newValue to replace the oldValue.
+                newValue.CopyTo(dstSpan[dstIdx..]);
+                dstIdx += newValue.Length;
+            }
+
+            // Copy over the final non-matching portion at the end
+            Debug.Assert(source.Length - thisIdx == dstSpan.Length - dstIdx);
+            source[thisIdx..].CopyTo(dstSpan[dstIdx..]);
+
+            replacementIndices.Dispose();
+
+            result = new ArrayPoolBuffer<T>(dest, length);
+            return true;
+        }
+    }
+}

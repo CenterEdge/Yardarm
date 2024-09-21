f875de51a1d2316cd8e91359a7b6fa1fdc258d43
Brant Burnett <bburnett@centeredgesoftware.com>
2024-09-21 17:30:16 -0400

    Load and parse embedded resources more efficiently
    Motivation
    ----------
    The current approach of a string replace incurs some additional
    allocation overhead.
    
    Modifications
    -------------
    Process the string replacement as UTF8 against the raw bytes of the
    manifest resource stream rather than deserializing to a string and then
    generating another string with the namespace replaced.

---
+++ src/main/Yardarm/Internal/ArrayPoolBuffer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Buffers;
+
+namespace Yardarm.Internal
+{
+    internal readonly ref struct ArrayPoolBuffer<T>
+    {
+        public T[]? Buffer { get; }
+        public int Length { get; }
+
+        public Span<T> Span => Buffer.AsSpan(0, Length);
+
+        public ArrayPoolBuffer(T[] buffer, int length)
+        {
+            ArgumentNullException.ThrowIfNull(buffer);
+            ArgumentOutOfRangeException.ThrowIfLessThan(length, 0);
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(length, buffer.Length);
+
+            Buffer = buffer;
+            Length = length;
+        }
+
+        public void Dispose()
+        {
+            if (Buffer is not null) // Maybe null if default struct
+            {
+                ArrayPool<T>.Shared.Return(Buffer);
+            }
+        }
+    }
+}

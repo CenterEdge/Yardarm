d15d4d949a2797ff739699439c87af10ee607569
Brant Burnett <bburnett@centeredgesoftware.com>
2024-10-11 14:07:33 -0400

    Update System.Text.Json to 8.0.5
    https://github.com/advisories/GHSA-8g4q-xg66-9fp4

--- src/main/Yardarm.SystemTextJson/JsonDependencyGenerator.cs
+++ src/main/Yardarm.SystemTextJson/JsonDependencyGenerator.cs
@@ -21,7 +21,7 @@ namespace Yardarm.SystemTextJson
                     {
                         Name = "System.Text.Json",
                         TypeConstraint = LibraryDependencyTarget.Package,
-                        VersionRange = VersionRange.Parse("8.0.4")
+                        VersionRange = VersionRange.Parse("8.0.5")
                     }
                 };
 
@@ -31,7 +31,7 @@ namespace Yardarm.SystemTextJson
                     {
                         Name = "System.Net.Http.Json",
                         TypeConstraint = LibraryDependencyTarget.Package,
-                        VersionRange = VersionRange.Parse("8.0.0")
+                        VersionRange = VersionRange.Parse("8.0.1")
                     }
                 };
             }

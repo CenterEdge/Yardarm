a000c31429543bf29dbc5362c132f70dbe231c50
Brant Burnett <bburnett@centeredgesoftware.com>
2024-10-13 13:30:17 -0300

    Use artifacts output path for build output (#264)
    * Use artifacts output path for build output
    
    * fix tests

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -5,12 +5,14 @@ set -e
 mkdir -p ./bin
 curl -sSL https://api.adv.centeredge.io/v1/swagger/api/swagger.json -o ./bin/mashtub.json
 
-dotnet run --no-build --no-launch-profile -c Release --project src/main/Yardarm.CommandLine -- \
-    restore -n TestSTJ -x src/main/Yardarm.SystemTextJson/bin/Release/net8.0/Yardarm.SystemTextJson.dll -f netstandard2.0 net6.0 net8.0 --intermediate-dir ./obj/
-dotnet run --no-build --no-launch-profile -c Release --project src/main/Yardarm.CommandLine -- \
-    generate --no-restore -n TestSTJ -x src/main/Yardarm.SystemTextJson/bin/Release/net8.0/Yardarm.SystemTextJson.dll -f netstandard2.0 net6.0 net8.0 --embed --intermediate-dir ./obj/ --nupkg ./bin/ -v 1.0.0 -i ./bin/mashtub.json
+httpext=src/artifacts/bin/Yardarm.MicrosoftExtensionsHttp/release/Yardarm.MicrosoftExtensionsHttp.dll
 
-dotnet run --no-build --no-launch-profile -c Release --project src/main/Yardarm.CommandLine -- \
-    restore -n TestNJ -x src/main/Yardarm.NewtonsoftJson/bin/Release/net8.0/Yardarm.NewtonsoftJson.dll -f netstandard2.0 net6.0 net8.0 --intermediate-dir ./obj/
-dotnet run --no-build --no-launch-profile -c Release --project src/main/Yardarm.CommandLine -- \
-    generate --no-restore -n TestNJ -x src/main/Yardarm.NewtonsoftJson/bin/Release/net8.0/Yardarm.NewtonsoftJson.dll -f netstandard2.0 net6.0 net8.0 --embed --intermediate-dir ./obj/ --nupkg ./bin/ -v 1.0.0 -i ./bin/mashtub.json
+for extension in "Yardarm.SystemTextJson" "Yardarm.NewtonsoftJson"; do
+    jsonext=src/artifacts/bin/$extension/release/$extension.dll
+
+    dotnet run --no-build --no-launch-profile -c Release --project src/main/Yardarm.CommandLine -- \
+    restore -n TestSTJ -x $jsonext $httpext -f netstandard2.0 net6.0 net8.0 --intermediate-dir ./obj/
+
+    dotnet run --no-build --no-launch-profile -c Release --project src/main/Yardarm.CommandLine -- \
+    generate --no-restore -n TestSTJ -x $jsonext $httpext -f netstandard2.0 net6.0 net8.0 --embed --intermediate-dir ./obj/ --nupkg ./bin/ -v 1.0.0 -i ./bin/mashtub.json
+done

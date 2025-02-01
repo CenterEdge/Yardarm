7c418a40f942806d45efcbae572458aba933fb14
Brant Burnett <bburnett@centeredgesoftware.com>
2025-02-01 12:33:26 -0500

    Add DateOnly and TimeOnly literal converters (#282)
    Motivation
    ----------
    We'd like to add support for DateOnly and TimeOnly when targeting modern .NET and using System.Text.Json. When we do, we'll need support for header and query string literals as well.
    
    Modifications
    -------------
    - Add DateOnlyLiteralConverter and TimeOnlyLiteralConverter, mirroring the serialization formats used by System.Text.Json
    - Register on LiteralConverterRegistry
    
    Results
    -------
    No changes for now unless an extension is used to change schema generation to use DateOnly and TimeOnly.

---
+++ src/main/Yardarm.Client/Serialization/Literals/Converters/TimeOnlyLiteralConverter.net6.0.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Yardarm.Client.Internal;
+
+namespace RootNamespace.Serialization.Literals.Converters;
+
+internal sealed class TimeOnlyLiteralConverter : ValueTypeLiteralConverter<TimeOnly>
+{
+    protected override TimeOnly ReadCore(string value, string? format)
+    {
+        ThrowHelper.ThrowIfNull(value);
+
+        char firstChar = value[0];
+        int firstSeparator = value.AsSpan().IndexOfAny('.', ':');
+        if (!char.IsDigit(firstChar) || firstSeparator < 0 || value[firstSeparator] == '.')
+        {
+            // Note: TimeSpan.ParseExact permits leading whitespace, negative values
+            // and numbers of days so we need to exclude these cases here.
+            ThrowHelper.ThrowFormatException("The value is not in a supported TimeOnly format.");
+        }
+
+        return TimeOnly.FromTimeSpan(TimeSpan.ParseExact(value, "c", null));
+    }
+
+    public override string Write(TimeOnly value, string? format) =>
+        value.ToTimeSpan().ToString("c");
+
+    public override bool TryWrite(TimeOnly value, ReadOnlySpan<char> format, Span<char> destination, out int charsWritten) =>
+        value.ToTimeSpan().TryFormat(destination, out charsWritten, "c");
+}

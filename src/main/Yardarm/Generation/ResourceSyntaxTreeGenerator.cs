7dd54f58ba9e525eaefefb97bd4e485f92ceb089
Brant Burnett <bburnett@centeredgesoftware.com>
2025-02-01 11:59:52 -0500

    Support C# resource filtering by .NET version
    Motivation
    ----------
    The current filtering approach is limited to only .NET Standard vs
    .NET Core and modern .NET. There are reasons to also allow filtering
    based on specific .NET versions.
    
    Modifications
    -------------
    - Move a lot of duplicate logic in the .Client projects to the
      Directory.Build.targets file.
    - Allow `.netX.Y.cs` suffixes on resource files in the .Client projects
      to limit the target versions they apply to.

--- src/main/Yardarm/Generation/ResourceSyntaxTreeGenerator.cs
+++ src/main/Yardarm/Generation/ResourceSyntaxTreeGenerator.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Text;
@@ -12,8 +14,29 @@ using Yardarm.Packaging;
 
 namespace Yardarm.Generation
 {
-    public abstract class ResourceSyntaxTreeGenerator : ISyntaxTreeGenerator
+    public abstract partial class ResourceSyntaxTreeGenerator : ISyntaxTreeGenerator
     {
+        [GeneratedRegex(@"\.netstandard\.cs$")]
+        private static partial Regex NetStandardSuffix();
+
+        [GeneratedRegex(@"\.netcoreapp\.cs$")]
+        private static partial Regex NetCoreAppSuffix();
+
+        [GeneratedRegex(@"\.net\d+\.\d+\.cs$")]
+        private static partial Regex AnyNetNumberSuffix();
+
+        [GeneratedRegex(@"\.net6\.0\.cs$")]
+        private static partial Regex Net60Suffix();
+
+        [GeneratedRegex(@"\.net7\.0\.cs$")]
+        private static partial Regex Net70Suffix();
+
+        [GeneratedRegex(@"\.net8\.0\.cs$")]
+        private static partial Regex Net80Suffix();
+
+        [GeneratedRegex(@"\.net9\.0\.cs$")]
+        private static partial Regex Net90Suffix();
+
         private static readonly UTF8Encoding s_utf8NoBom = new(encoderShouldEmitUTF8Identifier: false);
         private static ReadOnlySpan<byte> RootNamespaceBytes => "RootNamespace"u8;
 
@@ -31,19 +54,23 @@ namespace Yardarm.Generation
             RootNamespace = rootNamespace;
         }
 
+        public virtual IEnumerable<Regex> GetResourceNameExclusions() =>
+            GenerationContext.CurrentTargetFramework.Framework ==
+                                       NuGetFrameworkConstants.NetStandardFramework
+                ? [NetCoreAppSuffix(), AnyNetNumberSuffix()]
+                : [NetStandardSuffix(), .. GetNetVersionSuffixExclusions(GenerationContext.CurrentTargetFramework.Version)];
+
         public virtual IEnumerable<SyntaxTree> Generate()
         {
-            string excludeSuffix = GenerationContext.CurrentTargetFramework.Framework ==
-                                   NuGetFrameworkConstants.NetStandardFramework
-                ? ".netcoreapp.cs"
-                : ".netstandard.cs";
+            Regex[] excludeSuffixes = GetResourceNameExclusions().ToArray();
 
             byte[] namespaceName = s_utf8NoBom.GetBytes(RootNamespace.Name.ToString());
 
             var result = new List<SyntaxTree>();
             foreach (string resourceName in GetType().Assembly.GetManifestResourceNames())
             {
-                if (resourceName.StartsWith(ResourcePrefix) && resourceName.EndsWith(".cs") && !resourceName.EndsWith(excludeSuffix))
+                if (resourceName.StartsWith(ResourcePrefix) && resourceName.EndsWith(".cs") &&
+                    !IsAnyMatch(excludeSuffixes, resourceName))
                 {
                     result.Add(ParseResource(resourceName, namespaceName));
                 }
@@ -87,6 +114,39 @@ namespace Yardarm.Generation
                 syntaxTree.Options);
 
             return syntaxTree;
+        }
+
+        private static IEnumerable<Regex> GetNetVersionSuffixExclusions(Version version)
+        {
+            if (version.Major < 6)
+            {
+                yield return Net60Suffix();
+            }
+            if (version.Major < 7)
+            {
+                yield return Net70Suffix();
+            }
+            if (version.Major < 8)
+            {
+                yield return Net80Suffix();
+            }
+            if (version.Major < 9)
+            {
+                yield return Net90Suffix();
+            }
+        }
+
+        private static bool IsAnyMatch(Regex[] patterns, string input)
+        {
+            foreach (Regex pattern in patterns)
+            {
+                if (pattern.IsMatch(input))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }

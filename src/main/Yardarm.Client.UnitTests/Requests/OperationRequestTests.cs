e884b7c16f48b0cb454789aea9d9524010c6636a
Brant Burnett <bburnett@centeredgesoftware.com>
2026-02-17 08:22:44 -0500

    Allow consumers to apply HttpRequestOptions
    Motivation
    ----------
    This allows additional information to be applied to the outgoing
    HttpRequestMessage which can then be used by HttpMessageHandlers in the
    outgoing request pipeline to change request behaviors.
    
    Modifications
    -------------
    Add the options to IOperationRequest and OperationRequest when
    targeting .NET 5 or later. .NET 5 is required for HttpRequestOptions.

---
+++ src/main/Yardarm.Client.UnitTests/Requests/OperationRequestTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Http;
+using RootNamespace.Requests;
+using RootNamespace.Serialization;
+using Xunit;
+
+namespace Yardarm.Client.UnitTests.Requests;
+
+public class OperationRequestTests
+{
+    [Fact]
+    public void BuildRequest_AppliesOptions()
+    {
+        // Arrange
+
+        var request = new TestOperationRequest();
+        request.Options.Set(s_testOptionKey, "TestValue");
+
+        var context = new BuildRequestContext(TypeSerializerRegistry.CreateDefaultRegistry());
+
+        // Act
+
+        var requestMessage = request.BuildRequest(context);
+
+        // Assert
+
+        Assert.True(requestMessage.Options.TryGetValue(s_testOptionKey, out string value));
+        Assert.Equal("TestValue", value);
+    }
+
+    [Fact]
+    public void BuildRequest_SucceedsWithNoOptions()
+    {
+        // Arrange
+
+        var request = new TestOperationRequest();
+        var context = new BuildRequestContext(TypeSerializerRegistry.CreateDefaultRegistry());
+
+        // Act
+
+        var requestMessage = request.BuildRequest(context);
+
+        // Assert
+
+        Assert.False(requestMessage.Options.TryGetValue(s_testOptionKey, out _));
+    }
+
+    private sealed class TestOperationRequest : OperationRequest
+    {
+        protected override HttpMethod Method => HttpMethod.Get;
+        protected override Uri BuildUri(BuildRequestContext context) => new("https://example.com");
+    }
+
+    private static readonly HttpRequestOptionsKey<string> s_testOptionKey = new("TestOption");
+}
--- src/main/Yardarm.Client/Requests/IOperationRequest.cs
+++ src/main/Yardarm.Client/Requests/IOperationRequest.cs
@@ -1,28 +1,51 @@
 ﻿using RootNamespace.Authentication;
 using RootNamespace.Responses;
 
-namespace RootNamespace.Requests
-{
-    public interface IOperationRequest
-    {
-        /// <summary>
-        /// Optionally override the default authenticator for this request.
-        /// </summary>
-        public IAuthenticator? Authenticator { get; set; }
+#if NET5_0_OR_GREATER
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Threading;
+#endif
 
-        /// <summary>
-        /// If true, the request will return after response headers are received and the response body will be streamed.
-        /// </summary>
-        /// <remarks>
-        /// <para>
-        /// This is particularly useful for large response bodies. However, when using response streaming the response body
-        /// will not be buffered and may only be read once.
-        /// </para>
-        /// <para>
-        /// Additionally, it is imperative that <see cref="IOperationResponse.Dispose" /> be called when the response is no
-        /// longer needed to avoid leaking connections. This is particularly important for .NET 4.x consumers.
-        /// </para>
-        /// </remarks>
-        public bool EnableResponseStreaming { get; set; }
-    }
+namespace RootNamespace.Requests;
+
+public interface IOperationRequest
+{
+    /// <summary>
+    /// Optionally override the default authenticator for this request.
+    /// </summary>
+    public IAuthenticator? Authenticator { get; set; }
+
+    /// <summary>
+    /// If true, the request will return after response headers are received and the response body will be streamed.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This is particularly useful for large response bodies. However, when using response streaming the response body
+    /// will not be buffered and may only be read once.
+    /// </para>
+    /// <para>
+    /// Additionally, it is imperative that <see cref="IOperationResponse.Dispose" /> be called when the response is no
+    /// longer needed to avoid leaking connections. This is particularly important for .NET 4.x consumers.
+    /// </para>
+    /// </remarks>
+    public bool EnableResponseStreaming { get; set; }
+
+#if NET5_0_OR_GREATER
+    /// <summary>
+    /// Options to apply to the <see cref="HttpRequestMessage"/>.
+    /// </summary>
+    // This includes a DIM for backward compatibility, it should normally be unused.
+    public HttpRequestOptions Options => OperationRequestExtensions.OptionsTable.GetValue(this, static _ => []);
+#endif
 }
+
+#if NET5_0_OR_GREATER
+internal static class OperationRequestExtensions
+{
+    private static ConditionalWeakTable<IOperationRequest, HttpRequestOptions>? s_optionsTable = null;
+
+    public static ConditionalWeakTable<IOperationRequest, HttpRequestOptions> OptionsTable =>
+        s_optionsTable ?? Interlocked.CompareExchange(ref s_optionsTable, [], null) ?? s_optionsTable;
+}
+#endif

34140368f6c7c7459cc9a81e24ba12c79e15eb27
Brant Burnett <bburnett@centeredgesoftware.com>
2026-06-24 16:12:53 -0400

    Use RFC3339 for NodaTime OffsetTime serialization
    Motivation
    ----------
    OpenAPI 3.1 follows RFC3339 for date/time formats, which is based on
    ISO8601 but doesn't allow excluding minutes from the time zone offset
    such as `-04` instead of `-04:00`. The default NodaTime serializers for
    System.Text.Json do this for OffsetDateTime, but not for OffsetTime.
    This causes output serialized by the SDK to fail to deserialize when
    received by strict deserializers.
    
    Modifications
    -------------
    Add an intermediate JsonConvertFactory that returns a converter with an
    RFC3339 format string for OffsetTime. It forwards to the default
    implementation for all other NodaTime types.

---
+++ src/main/Yardarm.NodaTime.Client/Serialization/Json/YardarmNodaTimeJsonConverterFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using NodaTime;
+using NodaTime.Serialization.SystemTextJson;
+using NodaTime.Text;
+
+namespace RootNamespace.Serialization.Json;
+
+/// <summary>
+/// Extends <see cref="NodaTimeDefaultJsonConverterFactory"/> to support
+/// Yardarm-specific conversions.
+/// </summary>
+/// <remarks>
+/// In particular, uses RFC3339 style formatting for <see cref="OffsetTime"/> which always outputs the
+/// minutes of the time zone offset, even if they are zero. This is required for OpenAPI 3.1 compliance.
+/// </remarks>
+internal sealed class YardarmNodaTimeJsonConverterFactory : JsonConverterFactory
+{
+    public static JsonConverter<OffsetTime> OffsetTimeConverter { get; } =
+        new NodaPatternConverter<OffsetTime>(OffsetTimePattern.Rfc3339);
+
+    private readonly NodaTimeDefaultJsonConverterFactory _innerFactory = new();
+
+    public override bool CanConvert(Type typeToConvert)
+    {
+        if (typeToConvert == typeof(OffsetTime))
+        {
+            return true;
+        }
+
+        return _innerFactory.CanConvert(typeToConvert);
+    }
+
+    public override JsonConverter? CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (typeToConvert == typeof(OffsetTime))
+        {
+            return OffsetTimeConverter;
+        }
+
+        return _innerFactory.CreateConverter(typeToConvert, options);
+    }
+}

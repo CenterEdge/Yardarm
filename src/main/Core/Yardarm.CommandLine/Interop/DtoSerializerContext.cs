7c3c1e813b2d3de0aed46ba1c48a9716aa9f409a
Brant Burnett <bburnett@centeredgesoftware.com>
2026-08-10 08:23:02 -0400

    Move to .NET 10 for the SDK task (#350)
    Motivation
    ----------
    .NET 10 SDK and VS2026 and MSBuild 18 now allow the use of .NET 10
    tasks, even for builds from the CLR4 variant of MSBuild.
    
    Modifications
    -------------
    - Update Yardarm.Sdk to target .NET 10 only, dropping .NET 4.7.2 and
    updating from netcoreapp3.1
    - Register the tasks to run with TaskHostFactory when executing from a
    CLR4 variant of MSBuild
    - Move client dependendency versioning to a separate props file so we
    can have consistent versions with the Yardarm.Sdk.Test project
    - Cleanup conditional compilation

--- src/main/Core/Yardarm.CommandLine/Interop/DtoSerializerContext.cs
+++ src/main/Core/Yardarm.CommandLine/Interop/DtoSerializerContext.cs
@@ -1,6 +1,4 @@
-﻿#if NET6_0_OR_GREATER
-
-using System;
+﻿using System;
 using System.Text.Json.Serialization;
 
 #nullable enable
@@ -13,5 +11,3 @@ namespace Yardarm.CommandLine.Interop
     {
     }
 }
-
-#endif
